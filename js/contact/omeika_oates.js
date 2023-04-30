495ca4bc4629d9c1d54d5993474b181c62a58a4d
omeikagit <oatesomeika@gmail.com>
2023-04-30 15:11:28 -0500

    updates

--- js/contact/omeika_oates.js
+++ js/contact/omeika_oates.js
@@ -1,19 +1,35 @@
-function submitContactForm(event){
+//get contact form reference
+var contactForm = document.getElementById("contactForm");
 
+//add listener to watch for submit even on contact form and 
+//call submitContactForm function when it hears the event
+contactForm.addEventListener("submit", submitContactForm);
+
+//this function will determine what is done when submit event is triggered.
+function submitContactForm(event){
+    //don't do the default action
     event.preventDefault();
 
     console.log("submit contact form function triggered");
 
-    let firstname=document.getElementById("fname").Value;
-    console.log("First Name is: " + firstname);
+    //get user input from first name field
+    let firstName = document.getElementById("fname").value;
+    console.log("First name is: " + firstName);
 
-    let lastname =document.getElementById("fname").Value;
-    console.log("Last Name is: " + lastname);
+     //get user input from last name field
+    let lastName = document.getElementById("lname").value;
+    console.log("Last name is: " + lastName);
 
-    let email =document.getElementById("email").Value;
-    console.log("email is: " + email);
+     //get user input from email field
+    let email = document.getElementById("email").value;
+    console.log("Email is: " + email);
 
-    let contactnumber =document.getElementById("contact_number").Value;
-    console.log("contact number is: " + contactNumber);
+     //get user input from contact number field
+    let contactNumber = document.getElementById("contact_number").value;
+    console.log("Contact number is: " + contactNumber);
 
+     //get user input from subject field
+    let subject = document.getElementById("subject").value;
+    console.log("Subject is: " + subject);
 }
+
