e09db84982d1be8026c96236a7bf85bb0b912ce3
omeikagit <oatesomeika@gmail.com>
2023-04-30 14:51:41 -0500

    java script changes

--- js/contact/omeika_oates.js
+++ js/contact/omeika_oates.js
@@ -3,7 +3,17 @@ function submitContactForm(event){
     event.preventDefault();
 
     console.log("submit contact form function triggered");
+
     let firstname=document.getElementById("fname").Value;
     console.log("First Name is: " + firstname);
+
+    let lastname =document.getElementById("fname").Value;
+    console.log("Last Name is: " + lastname);
+
+    let email =document.getElementById("email").Value;
+    console.log("email is: " + email);
+
+    let contactnumber =document.getElementById("contact_number").Value;
+    console.log("contact number is: " + contactNumber);
 
 }
